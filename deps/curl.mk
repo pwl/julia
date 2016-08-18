d6dffe7da5b6bb185b7db97598f24c7510479e7a
Viral B. Shah <viral@mayin.org>
2016-08-18 09:20:45 +0530

    Explicitly disable installing zsh completions.

--- deps/curl.mk
+++ deps/curl.mk
@@ -18,7 +18,7 @@ $(SRCDIR)/srccache/curl-$(CURL_VER)/configure: $(SRCDIR)/srccache/curl-$(CURL_VE
 $(BUILDDIR)/curl-$(CURL_VER)/config.status: $(SRCDIR)/srccache/curl-$(CURL_VER)/configure
 	mkdir -p $(dir $@)
 	cd $(dir $@) && \
-	$< $(CONFIGURE_COMMON) --includedir=$(build_includedir) --disable-silent-rules --without-gnutls --without-gssapi --without-libidn --without-libmetalink --without-librtmp --without-libssh2 --without-nghttp2 --without-nss --without-polarssl --without-spnego --without-ssl --disable-ares --disable-ldap --disable-ldaps --with-mbedtls=$(build_prefix) CFLAGS="$(CFLAGS) $(CURL_CFLAGS)" LDFLAGS="$(LDFLAGS) $(CURL_LDFLAGS)"
+	$< $(CONFIGURE_COMMON) --includedir=$(build_includedir) --disable-silent-rules --without-gnutls --without-gssapi --without-libidn --without-libmetalink --without-librtmp --without-libssh2 --without-nghttp2 --without-nss --without-polarssl --without-spnego --without-ssl --disable-ares --disable-ldap --disable-ldaps --without-zsh-functions-dir --with-mbedtls=$(build_prefix) CFLAGS="$(CFLAGS) $(CURL_CFLAGS)" LDFLAGS="$(LDFLAGS) $(CURL_LDFLAGS)"
 	touch -c $@
 
 $(CURL_SRC_TARGET): $(BUILDDIR)/curl-$(CURL_VER)/config.status
